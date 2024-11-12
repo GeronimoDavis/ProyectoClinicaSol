b14ea2a9b30633d0e8ea610758df6c8f3f900e56
Maximiliano Agustín Especie Calcagno <56851501+maximilianocalcagno@users.noreply.github.com>
2024-11-12 01:17:39 -0300

    Vista para crear paciente

--- ProyectoClinica/Form3.Designer.cs
+++ ProyectoClinica/Form3.Designer.cs
@@ -28,12 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form3";
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label7 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(198, 31);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Nombre";
+            textBox1.Size = new Size(154, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(120, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Nombre";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(198, 60);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Nombre";
+            textBox2.Size = new Size(154, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(198, 89);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Nombre";
+            textBox3.Size = new Size(154, 23);
+            textBox3.TabIndex = 3;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(198, 118);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "Nombre";
+            textBox4.Size = new Size(154, 23);
+            textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(198, 147);
+            textBox5.Name = "textBox5";
+            textBox5.PlaceholderText = "Nombre";
+            textBox5.Size = new Size(154, 23);
+            textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(198, 205);
+            textBox6.Name = "textBox6";
+            textBox6.PlaceholderText = "Nombre";
+            textBox6.Size = new Size(154, 23);
+            textBox6.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(120, 68);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(120, 97);
+            label3.Name = "label3";
+            label3.Size = new Size(27, 15);
+            label3.TabIndex = 9;
+            label3.Text = "DNI";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(120, 126);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Telefono";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(120, 155);
+            label5.Name = "label5";
+            label5.Size = new Size(44, 15);
+            label5.TabIndex = 11;
+            label5.Text = "Celular";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(113, 213);
+            label6.Name = "label6";
+            label6.Size = new Size(51, 15);
+            label6.TabIndex = 12;
+            label6.Text = "Nombre";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(198, 176);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(154, 23);
+            dateTimePicker1.TabIndex = 13;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(75, 184);
+            label7.Name = "label7";
+            label7.Size = new Size(117, 15);
+            label7.TabIndex = 14;
+            label7.Text = "Fecha de nacimiento";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(182, 263);
+            button1.Name = "button1";
+            button1.Size = new Size(158, 29);
+            button1.TabIndex = 15;
+            button1.Text = "Crear paciente";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label7);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form3";
+            Text = "Form3";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private DateTimePicker dateTimePicker1;
+        private Label label7;
+        private Button button1;
     }
 }
--- ProyectoClinica/Form3.cs
+++ ProyectoClinica/Form3.cs
@@ -10,11 +10,26 @@ using System.Windows.Forms;
 
 namespace ProyectoClinica
 {
-    public partial class Form3: Form
+    public partial class Form3 : Form
     {
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
