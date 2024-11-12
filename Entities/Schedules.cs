e1136ba4c1301924092ebeb1df49c8878e002782
Maximiliano Agustín Especie Calcagno <56851501+maximilianocalcagno@users.noreply.github.com>
2024-11-12 20:00:29 -0300

    Creada la clase Schedules en la capa Entities

---
+++ Entities/Schedules.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class Schedules
+    {
+        public int ScheduleId { get; set; }
+        public int Day {  get; set; }
+        public DateTime Time { get; set; }
+        public int ProfessionalID { get; set; }
+
+        public Schedules() { }
+
+        public Schedules(int scheduleId, int day, DateTime time, int professionalID) 
+        {
+            ScheduleId = scheduleId;
+            Day = day;
+            Time = time;
+            ProfessionalID = professionalID;
+        }
+    }
+}
