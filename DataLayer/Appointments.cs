e27256fe9f5f0113af0e6918f979f129076abcc4
Maximiliano Agustín Especie Calcagno <56851501+maximilianocalcagno@users.noreply.github.com>
2024-11-12 21:03:27 -0300

    Modificados los atributos de la clase Appointments

---
+++ DataLayer/Appointments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    public class Appointments
+    {
+        public static void CreateAppointments(Appointments appointments)
+        {
+
+        }
+    }
+}
--- Entities/Appointments.cs
+++ Entities/Appointments.cs
@@ -8,19 +8,23 @@ namespace Entities
 {
     public class Appointments
     {
-        public int ScheludeId { get; set; }
-        public int Day { get; set; }
+        public int AppointmentId { get; set; }
+        public int PatientId { get; set; }
         public DateTime Time { get; set; }
         public int ProfessionalId { get; set; }
+        public bool Canceled { get; set; }
+        public string Status { get; set; }
 
         public Appointments() { }
 
-        public Appointments(int scheludeId, int day, DateTime time, int professionalId) 
+        public Appointments(int appointmentId, int patientId, DateTime time, int professionalId, bool canceled, string status) 
         {
-            ScheludeId = scheludeId;
-            Day = day;
+            AppointmentId = appointmentId;
+            PatientId = patientId;
             Time = time;
             ProfessionalId = professionalId;
+            Canceled = canceled;
+            Status = status;
         }
     }
 }
